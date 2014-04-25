503fb1bb01c48a773fd7c32306d6efe6e4728bf2
Māris Krivtežs <marisks@gmail.com>
2014-04-25 23:58:55 +0300

    First slides and sample project

---
+++ code/Tests/Tests/MathTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xunit;
+
+namespace Tests
+{
+    public class MathTests
+    {
+        [Fact]
+        public void two_plus_two_is_four()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+
+    public class SetupTeardownTests : IDisposable
+    {
+        private int _resource;
+
+        public SetupTeardownTests()
+        {
+            _resource = 5;
+        }
+
+        [Fact]
+        public void resource_is_initialized()
+        {
+            Assert.Equal(_resource, 5);
+        }
+
+        public void Dispose()
+        {
+            _resource = 0;
+        }
+    }
+}
